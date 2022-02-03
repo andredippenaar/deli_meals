282ae2268f7000082802c0866ba03a49195ad020
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2022-02-03 11:09:40 +0200

    Styling and theming

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -7,8 +7,11 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('DeliMeal'),),
+      appBar: AppBar(
+        title: const Text('DeliMeal'),
+      ),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map(
               (catData) => CategoryItem(catData.title, catData.color),
